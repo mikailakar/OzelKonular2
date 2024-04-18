af3b7a46d476cd9adf3814af3e69acfb5b527c1e
Mikail Akar <huseyin.akar@bil.omu.edu.tr>
2024-04-18 18:51:48 +0300

    sifreleme

---
+++ SifrelemeSistemi/ok2/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace ok2 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.key = new System.Windows.Forms.TextBox();
+            this.input = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.encryptd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.decryptd = new System.Windows.Forms.TextBox();
+            this.input2 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(33, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anahtar";
+            // 
+            // key
+            // 
+            this.key.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.key.Location = new System.Drawing.Point(143, 100);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(85, 30);
+            this.key.TabIndex = 1;
+            this.key.TextChanged += new System.EventHandler(this.key_TextChanged);
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.input.Location = new System.Drawing.Point(130, 100);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(590, 90);
+            this.input.TabIndex = 3;
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(20, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Metin";
+            // 
+            // encryptd
+            // 
+            this.encryptd.AccessibleDescription = "";
+            this.encryptd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.encryptd.Location = new System.Drawing.Point(130, 220);
+            this.encryptd.Multiline = true;
+            this.encryptd.Name = "encryptd";
+            this.encryptd.Size = new System.Drawing.Size(590, 90);
+            this.encryptd.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(20, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Şifreli";
+            // 
+            // decryptd
+            // 
+            this.decryptd.AccessibleDescription = "";
+            this.decryptd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.decryptd.Location = new System.Drawing.Point(130, 220);
+            this.decryptd.Multiline = true;
+            this.decryptd.Name = "decryptd";
+            this.decryptd.Size = new System.Drawing.Size(590, 90);
+            this.decryptd.TabIndex = 9;
+            // 
+            // input2
+            // 
+            this.input2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.input2.Location = new System.Drawing.Point(130, 100);
+            this.input2.Multiline = true;
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(590, 90);
+            this.input2.TabIndex = 8;
+            this.input2.Click += new System.EventHandler(this.input2_Click);
+            this.input2.TextChanged += new System.EventHandler(this.input2_TextChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.tabControl1.ItemSize = new System.Drawing.Size(80, 50);
+            this.tabControl1.Location = new System.Drawing.Point(10, 10);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Padding = new System.Drawing.Point(158, 3);
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(760, 410);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.DimGray;
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.input);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.encryptd);
+            this.tabPage1.Location = new System.Drawing.Point(4, 54);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(752, 352);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Şifrele";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.DimGray;
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.input2);
+            this.tabPage2.Controls.Add(this.decryptd);
+            this.tabPage2.Location = new System.Drawing.Point(4, 54);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(752, 342);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Deşifre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(20, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Şifreli";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label5.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Location = new System.Drawing.Point(20, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Deşifre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 431);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Şifreleme";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox encryptd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox decryptd;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ SifrelemeSistemi/ok2/Form1.cs
@@ -0,0 +1,434 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ok2 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            encryptd.ReadOnly = true;
+            decryptd.ReadOnly = true;
+        }
+
+        string lastkey;
+        string lastinput2;
+
+        private void input_TextChanged(object sender, EventArgs e) {
+            try {
+                updsif();
+            } catch (Exception ex) {
+            }
+        }
+
+        private void input2_TextChanged(object sender, EventArgs e) {
+            try {
+                upddes();
+            } catch (Exception ex) {
+                if (input2.Text != "") { input2.Text = lastinput2;}
+            }
+            lastinput2 = input2.Text;
+        }
+
+        private void key_TextChanged(object sender, EventArgs e) {
+            try {
+                updsif();
+                upddes();
+            } catch (Exception ex) {
+                if (key.Text != "") {key.Text = lastkey;}       
+            }
+            if(key.Text.Select(x => Convert.ToInt32(x.ToString())).ToList().Count > 7) {key.Text = lastkey;}
+            lastkey = key.Text;
+        }
+
+        private void updsif() {
+            mapmaker(Convert.ToInt32(key.Text));
+            List<int> encryptedd = encrypt(input.Text);
+            encryptd.Text = string.Join("", encryptedd);
+        }
+
+        private void upddes() {
+            mapmaker(Convert.ToInt32(key.Text));
+            List<char> decryptedd = decrypt(input2.Text.Select(x => Convert.ToInt32(x.ToString())).ToList());
+            decryptd.Text = string.Join("", decryptedd);
+        }
+
+        private void input2_Click(object sender, EventArgs e) {
+            input2.Text = encryptd.Text;
+        }
+
+        int x, y, z;
+        int xSize = 4, ySize = 3, zSize = 3, keySize = 7;
+
+        char[] kar = { 'a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g', 'ğ', 'h', 'i', 'ı', 'j', 'k', 'l', 'm', 'n', 'o', 'ö', 'p', 'q', 'r', 's', 'ş', 't', 'u', 'ü', 'v', 'w', 'x', 'y', 'z', '.', ',', ' ' };
+
+        char[] c1 = { '0', '0', '0', '0' };
+        char[] c2 = { '0', '0', '0', '0' };
+        char[] c3 = { '0', '0', '0', '0' };
+        char[] c4 = { '0', '0', '0', '0' };
+        char[] c5 = { '0', '0', '0', '0' };
+        char[] c6 = { '0', '0', '0', '0' };
+        char[] c7 = { '0', '0', '0', '0' };
+        char[] c8 = { '0', '0', '0', '0' };
+        char[] c9 = { '0', '0', '0', '0' };
+
+
+        private void reset() {
+            x = 0;
+            y = 0;
+            z = 0;
+            int ccount = 0;
+            foreach (char cc in c1) {
+                c1[ccount] = '0';
+                c2[ccount] = '0';
+                c3[ccount] = '0';
+                c4[ccount] = '0';
+                c5[ccount] = '0';
+                c6[ccount] = '0';
+                c7[ccount] = '0';
+                c8[ccount] = '0';
+                c9[ccount] = '0';
+                ccount++;
+            }
+        }
+
+        private void mapmaker(int key) {
+            reset();
+            int l = 0;
+            foreach (char a in kar) {
+                x = xyz(key, l, 0);
+                y = xyz(key, l, 1);
+                z = xyz(key, l, 2);
+
+                x = x % xSize;
+                y = y % ySize;
+                z = z % zSize;
+
+                l++;
+
+                put(a, x, y, z, 0, 0);
+            }
+        }
+
+        private int xyz(int key, int tour, int ind) {
+            int xr1 = key / 1000000;
+            int xr2 = key / 100000 - xr1 * 10;
+            int xr3 = key / 10000 - xr1 * 100 - xr2 * 10;
+            int xr4 = key / 1000 - xr1 * 1000 - xr2 * 100 - xr3 * 10;
+            int xr5 = key / 100 - xr1 * 10000 - xr2 * 1000 - xr3 * 100 - xr4 * 10;
+            int xr6 = key / 10 - xr1 * 100000 - xr2 * 10000 - xr3 * 1000 - xr4 * 100 - xr5 * 10;
+            int xr7 = key - xr1 * 1000000 - xr2 * 100000 - xr3 * 10000 - xr4 * 1000 - xr5 * 100 - xr6 * 10;
+
+            int lt = 0;
+            for (int xt = 1; xt <= keySize - 2; xt++) {
+                for (int yt = xt+1; yt <= keySize - 1; yt++) {
+                    for (int zt = yt+1; zt <= keySize; zt++) {
+                        if (lt == tour) {
+                            if (ind == 0) {
+                                switch (xt) {
+                                    case 1:
+                                        return xr1;
+                                    case 2:
+                                        return xr2;
+                                    case 3:
+                                        return xr3;
+                                    case 4:
+                                        return xr4;
+                                    case 5:
+                                        return xr5;
+                                }
+                            } else if (ind == 1) {
+                                switch (yt) {
+                                    case 2:
+                                        return xr2;
+                                    case 3:
+                                        return xr3;
+                                    case 4:
+                                        return xr4;
+                                    case 5:
+                                        return xr5;
+                                    case 6:
+                                        return xr6;
+                                }
+                            } else if (ind == 2) {
+                                switch (zt) {
+                                    case 3:
+                                        return xr3;
+                                    case 4:
+                                        return xr4;
+                                    case 5:
+                                        return xr5;
+                                    case 6:
+                                        return xr6;
+                                    case 7:
+                                        return xr7;
+                                }
+                            }
+                        }
+                        lt++;
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        private void put(char a, int x3, int y3, int z3, int t, int t2) {
+            int l = 0;
+
+            if (y3 == 0) {
+                if (z3 == 0) {
+                    if (c1[x3] == '0') {
+                        c1[x3] = a;
+                    } else {
+                        l = 1;
+                    }
+                } else if (z3 == 1) {
+                    if (c4[x3] == '0') {
+                        c4[x3] = a;
+                    } else {
+                        l = 1;
+                    }
+                } else if (z3 == 2) {
+                    if (c7[x3] == '0') {
+                        c7[x3] = a;
+                    } else {
+                        l = 1;
+                    }
+                }
+            } else if (y3 == 1) {
+                if (z3 == 0) {
+                    if (c2[x3] == '0') {
+                        c2[x3] = a;
+                    } else {
+                        l = 1;
+                    }
+                } else if (z3 == 1) {
+                    if (c5[x3] == '0') {
+                        c5[x3] = a;
+                    } else {
+                        l = 1;
+                    }
+                } else if (z3 == 2) {
+                    if (c8[x3] == '0') {
+                        c8[x3] = a;
+                    } else {
+                        l = 1;
+                    }
+                }
+            } else if (y3 == 2) {
+                if (z3 == 0) {
+                    if (c3[x3] == '0') {
+                        c3[x3] = a;
+                    } else {
+                        l = 1;
+                    }
+                } else if (z3 == 1) {
+                    if (c6[x3] == '0') {
+                        c6[x3] = a;
+                    } else {
+                        l = 1;
+                    }
+                } else if (z3 == 2) {
+                    if (c9[x3] == '0') {
+                        c9[x3] = a;
+                    } else {
+                        l = 1;
+                    }
+                }
+            }
+
+            if (l == 1) {
+                int t3 = 0;
+                int t4 = 0;
+                if (t2 == 0) {
+                    if (t < 3) {
+                        t3 = t + 1;
+                        t4 = t2;
+                        x3 = (x3 + 1) % xSize;
+                    } else {
+                        t3 = 0;
+                        t4 = t2 + 1;
+                        x3 = (x3 + 1) % xSize;
+                        y3 = (y3 + 1) % ySize;
+                    }
+                } else if (t2 == 1) {
+                    if (t < 3) {
+                        t3 = t + 1;
+                        t4 = t2;
+                        x3 = (x3 + 1) % xSize;
+                    } else {
+                        t3 = 0;
+                        t4 = t2 + 1;
+                        x3 = (x3 + 1) % xSize;
+                        y3 = (y3 + 1) % ySize;
+                    }
+                } else if (t2 == 2) {
+                    if (t < 3) {
+                        t3 = t + 1;
+                        t4 = t2;
+                        x3 = (x3 + 1) % xSize;
+                    } else {
+                        t3 = 0;
+                        t4 = 0;
+                        x3 = (x3 + 1) % xSize;
+                        y3 = (y3 + 1) % ySize;
+                        z3 = (z3 + 1) % zSize;
+                    }
+                }
+                put(a, x3, y3, z3, t3, t4);
+            }
+        }
+
+        void printcub() {
+            Console.Write("\nc1:");
+            c1.ToList().ForEach(i => Console.Write(" " + i));
+            Console.Write("\nc2:");
+            c2.ToList().ForEach(i => Console.Write(" " + i));
+            Console.Write("\nc3:");
+            c3.ToList().ForEach(i => Console.Write(" " + i));
+            Console.Write("\nc4:");
+            c4.ToList().ForEach(i => Console.Write(" " + i));
+            Console.Write("\nc5:");
+            c5.ToList().ForEach(i => Console.Write(" " + i));
+            Console.Write("\nc6:");
+            c6.ToList().ForEach(i => Console.Write(" " + i));
+            Console.Write("\nc7:");
+            c7.ToList().ForEach(i => Console.Write(" " + i));
+            Console.Write("\nc8:");
+            c8.ToList().ForEach(i => Console.Write(" " + i));
+            Console.Write("\nc9:");
+            c9.ToList().ForEach(i => Console.Write(" " + i));
+        }
+
+        private List<int> encrypt(string entry) {
+            List<int> encrypted = new List<int>();
+            int lxe = 0;
+            int lye = 0;
+            int lze = 0;
+
+            foreach (char cht in entry) {
+
+                int degage = 0;
+
+                for (int xe = 0; xe < xSize; xe++) {
+                    for (int ye = 0; ye < ySize; ye++) {
+                        for (int ze = 0; ze < zSize; ze++) {
+                            int nxe = (lxe + xe) % xSize;
+                            int nye = (lye + ye) % ySize;
+                            int nze = (lze + ze) % zSize;
+
+                            char wac = '0';
+
+                            if (nze == 0) {
+                                if (nye == 0) {
+                                    wac = c1[nxe];
+                                } else if (nye == 1) {
+                                    wac = c2[nxe];
+                                } else if (nye == 2) {
+                                    wac = c3[nxe];
+                                }
+                            } else if (nze == 1) {
+                                if (nye == 0) {
+                                    wac = c4[nxe];
+                                } else if (nye == 1) {
+                                    wac = c5[nxe];
+                                } else if (nye == 2) {
+                                    wac = c6[nxe];
+                                }
+                            } else if (nze == 2) {
+                                if (nye == 0) {
+                                    wac = c7[nxe];
+                                } else if (nye == 1) {
+                                    wac = c8[nxe];
+                                } else if (nye == 2) {
+                                    wac = c9[nxe];
+                                }
+                            }
+
+                            if (wac == cht) {
+                                encrypted.Add(xe);
+                                encrypted.Add(ye);
+                                encrypted.Add(ze);
+
+                                lxe = nxe;
+                                lye = nye;
+                                lze = nze;
+
+                                degage = 1;
+                                break;
+                            }
+                        }
+                        if (degage == 1) {
+                            break;
+                        }
+                    }
+                    if (degage == 1) {
+                        break;
+                    }
+                }
+            }
+            return encrypted;
+        }
+
+
+        private List<char> decrypt(List<int> dc) {
+            List<char> decrypted = new List<char>();
+            int lxe = 0;
+            int lye = 0;
+            int lze = 0;
+            for (int dt = 0; dt < dc.Count; dt += 3) {
+                char decr = '0';
+                int dx = dc[dt];
+                int dy = dx;
+                int dz = dx;
+                if (dt + 1 < dc.Count) {dy = dc[dt + 1];}
+                if (dt + 2 < dc.Count) {dz = dc[dt + 2];}
+                    
+
+                int nxe = (lxe + dx) % xSize;
+                int nye = (lye + dy) % ySize;
+                int nze = (lze + dz) % zSize;
+
+                if (nze == 0) {
+                    if (nye == 0) {
+                        decr = c1[nxe];
+                    } else if (nye == 1) {
+                        decr = c2[nxe];
+                    } else if (nye == 2) {
+                        decr = c3[nxe];
+                    }
+                } else if (nze == 1) {
+                    if (nye == 0) {
+                        decr = c4[nxe];
+                    } else if (nye == 1) {
+                        decr = c5[nxe];
+                    } else if (nye == 2) {
+                        decr = c6[nxe];
+                    }
+                } else if (nze == 2) {
+                    if (nye == 0) {
+                        decr = c7[nxe];
+                    } else if (nye == 1) {
+                        decr = c8[nxe];
+                    } else if (nye == 2) {
+                        decr = c9[nxe];
+                    }
+                }
+
+                lxe = nxe;
+                lye = nye;
+                lze = nze;
+
+                decrypted.Add(decr);
+            }
+            return decrypted;
+        }
+
+    }
+}
